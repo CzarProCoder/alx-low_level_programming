a3c54d0e2f90f4c8908e70c50c949bed0317aecf
CzarProCoder <juliusczar36@gmail.com>
2022-12-12 12:27:19 -0800

    Created file 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,19 +3,22 @@
 /**
  * main - Entry point
  * i - Number from 0 to 9
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int i;
-
-	for (i = '0'; i <= '9'; i++)
+	
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i);
-		putchar(", ");
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('$');
 	putchar('\n');
 	return (0);
 }
