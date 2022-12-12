6ac915243677a50747c6f5f78d5d9c2c1f0667c0
CzarProCoder <juliusczar36@gmail.com>
2022-12-12 07:06:56 -0800

    Created the 6-print_numbers.c file

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 0; ch <= 9; ch++)
+		putchar(ch);
+	putchar('\n');
+	return(0);
+}
