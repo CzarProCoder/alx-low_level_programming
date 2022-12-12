a4b7db324747a11b431ce0f45831faa6c40e7e68
CzarProCoder <juliusczar36@gmail.com>
2022-12-12 11:28:41 -0800

    Created the file 8-print-base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * dg - A digit vaue from 0 to 9
+ * ch - An alphabet from a to f
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+	char dg;
+	char ch;
+
+	for (dg = '0'; dg <= '9'; dg++)
+		putchar(dg);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
