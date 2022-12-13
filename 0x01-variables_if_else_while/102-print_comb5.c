bd19010a77bd3644b109da5761cfca2011ffa3c1
CzarProCoder <juliusczar36@gmail.com>
2022-12-13 06:32:56 -0800

    Created 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int y;
+	int z;
+
+	for (a = '0'; a <= 9; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (y = '0'; y <= '9'; y++)
+			{
+				for (z = (y + 1); z <= '9'; z++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(y);
+					putchar(z);
+					if (((a != '9') && (b != '8')) && ((y != '9') || (y != '9')))
+						putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
