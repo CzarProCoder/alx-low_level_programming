55250ff13a68ae464546fc8385a5676814bf8762
CzarProCoder <juliusczar36@gmail.com>
2022-12-15 12:50:08 -0800

    Created the 3-islower.c file in the 3 functons and nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Function to check for letter
+ * Return: 1 If the character is letter(s)
+ * @c: A character to be evaluated
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || 
+			((c >= 'A') && (c <= 'Z')))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
