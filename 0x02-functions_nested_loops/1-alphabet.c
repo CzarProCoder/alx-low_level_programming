845170950fffda36ac328f8a5e6882706f236cfa
CzarProCoder <juliusczar36@gmail.com>
2022-12-15 08:01:45 -0800

    Created the 1-alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main -
+ * Return: 
+ **/
+
+int main(void)
+{
+	char ch;
+	ch = 'a'
+
+	while ch <= 'z'
+	{
+		_putchar(ch);
+		ch++
+	}
+	putchar('\n');
+	
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,5 @@
  */
 
 char _putchar(char c);
+
+void print_alphabet(void);
