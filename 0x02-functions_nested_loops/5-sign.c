311cdeadd1e6c9de969243c5e912613b34883d8e
CzarProCoder <juliusczar36@gmail.com>
2022-12-15 13:18:58 -0800

    Created the 4-isalpha.c file in the 3 functons and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include main.h
+
+/**
+ * print_sign - Function to determine the sign of a value
+ * @n: Character to be evaluate
+ * Return: Can be 0, 1 or -1 depending on the sign
+ */
+
+int print_sign(int n)
+{
+	if (c > o)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+
+		return (-1)
+	}
+}
