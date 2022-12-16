00b3bab711a81b170f0faa5b1853f6f48f2e41fb
CzarProCoder <juliusczar36@gmail.com>
2022-12-16 02:28:36 -0800

    Created 7-print_last_digit.c and 8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,14 @@
 /**
  * print_last_digit - Function to print the last digit
  * @n: Integer to be evaluate
- * @last_digit: The last digit of a number
+ * Return: Returns the last digit
  */
 
 int print_last_digit(int n)
 {
-	int last_digit = (n % 10);
+	int last_digit;
+
+	last_digit = (n % 10);
 
 	if (last_digit < 0)
 		last_digit *= -1;
