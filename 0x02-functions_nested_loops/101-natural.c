e5423a1f06d20c17abc2339822c6d61436941d98
CzarProCoder <juliusczar36@gmail.com>
2022-12-16 07:14:09 -0800

    Fucntion to calculate sum of multiple of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int sum;
+
+	a = 0;
+
+	while (a <= 1024)
+	{
+		if (((a % 3) == 0) || ((a % 5) == 0))
+			sum += a;
+		a++;
+	}
+	printf("%d\n", sum);
+	
+	return (0);
+}
