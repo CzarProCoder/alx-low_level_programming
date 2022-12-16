5eef3a2d29681db762398149ec111bb34761d7fb
CzarProCoder <juliusczar36@gmail.com>
2022-12-16 03:24:28 -0800

    Function to calculate the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function to add 2 integers
+ * @a - First integer
+ * @b - Second integer
+ * Return - The result of addition
+ */
+
+int add(int a, int b)
+{
+	return (a+b);
+}
