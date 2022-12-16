8fb7d1229ab2bfcd7bc5de4aa43668cf65d4af72
CzarProCoder <juliusczar36@gmail.com>
2022-12-16 01:22:16 -0800

    Created 7-print_last_digit.c and 8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
- * 
- *
+ * print_last_digit - Function to print the last digit
+ * @n: Integer to be evaluate
+ * @last_digit: The last digit of a number
  */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = (n % 10);
+
+	_putchar(last_digit);
+
+	return (last_digit);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * 
+ *
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int y;
+	int z;
+
+	a = 0;
+
+	while (a <= 2)
+	{
+		b = 0;
+
+		while (b <= 3)
+		{
+			y <= 0;
+
+			while (y <= 5)
+			{
+				z = 0;
+				while (z <= 9)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':')
+					_putchar(y);
+					_putchar(z);
+					z++;
+				}
+				y++;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (0);
+
+}
