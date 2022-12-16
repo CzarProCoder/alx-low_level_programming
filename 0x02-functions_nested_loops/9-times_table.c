2ffe5aedff360915eef62a6a0e2b4b88f9ec9fcd
CzarProCoder <juliusczar36@gmail.com>
2022-12-16 03:11:15 -0800

    Created the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+/**
+ * times_table - Fucntion to generate 9 times table
+ * Return: Does not return a value
+ */
+
 void times_table(void)
 {
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0);
+		_putchar('0');
 		
 		for (mult = 1; mult <= 9; mult++)
 		{
