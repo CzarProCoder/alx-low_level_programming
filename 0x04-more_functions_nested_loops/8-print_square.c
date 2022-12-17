a606a9196f87c3298176b7b2e89b021dbfcb3c52
CzarProCoder <juliusczar36@gmail.com>
2022-12-17 05:33:01 -0800

    Created 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+/**
+ * print_square - Draw a square using #
+ * @size: Size of the Square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	else
+		_putchar('\n');
+}
