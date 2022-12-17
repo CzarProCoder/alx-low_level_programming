1a09dbe076bead212c7ee3a99b2fd8f0dc70f33e
CzarProCoder <juliusczar36@gmail.com>
2022-12-17 09:20:08 -0800

    Fucntion to print triangle of n sides

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+/**
+ * print_triangle - Function to print a triangle
+ * @size: Size of the triangle
+ * Return: Always 0 (Success)
+ */
+
+int print_triangle(int size)
+{
+	int n;
+	int c;
+
+	if (size > 0)
+	{
+		for (n = 1; n <= size; n++)
+		{
+			for (c = 1; c <= n; c++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
