d33bbaf98aa729584124c9531ca35f557c8ca5e1
CzarProCoder <juliusczar36@gmail.com>
2022-12-17 04:39:25 -0800

    Created file 7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function to draws a straight line in the terminal
+ * @n: Numbers of times to print character \
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int c;
+
+	if (n > 0)
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar('\');
+			_putchar('\n');
+		}
+
+	}
+	else
+		_putchar('\n');
+}
