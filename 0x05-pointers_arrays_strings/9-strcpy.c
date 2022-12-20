d4658db6cf9833948b40243a21bbfb6480f0a1fd
CzarProCoder <juliusczar36@gmail.com>
2022-12-20 13:45:55 -0800

    Function that copies the string pointed to by src, including the terminating null byte

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Function to copy string
+ * @dest: Destination buffer
+ * @src: source array
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*src++)
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	return (*dest);
+}
