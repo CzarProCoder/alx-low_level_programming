afe596e4387ec2721aa5a391e96d2d997e11295b
CzarProCoder <juliusczar36@gmail.com>
2022-12-23 13:44:01 -0800

    Created the 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,14 +19,14 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; src[i] != '\n'; i++)	/*Calculate number of words on src string*/
 		src_count++;
 
-	if src_count >= n	/* */
+	if (src_count >= n)
 		for (j = 0; j <= n; j++)
 			dest[j] = src[j];
 	else
 	{
 		for (j = 0; j <= src_count; j++)
 			dest[j] = src[j];
-		while src_count <= n
+		while (src_count <= n)
 			{
 				src_count++;
 				dest[src_count] = '\n';
