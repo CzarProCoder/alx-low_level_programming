326ee91bb709875a2e64b97494921f4735218e84
CzarProCoder <juliusczar36@gmail.com>
2022-12-23 13:58:16 -0800

    Created the 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,27 +5,37 @@
  * @dest: Destination string array
  * @src: Source string array
  * @n: Numbers of bytes
- * Return:
+ * Return: Pointer to the dest string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	int j;
+	int m;
 	int src_count;
 
 	src_count = 0;
 
-	for (i = 0; src[i] != '\n'; i++)	/*Calculate number of words on src string*/
+	for (i = 0; src[i] != '\n'; i++)	/*Calculate number of characters in src string*/
+	{
 		src_count++;
-
+	}
+	
 	if (src_count >= n)
+	{
 		for (j = 0; j <= n; j++)
+		{
 			dest[j] = src[j];
+		}
+	}
 	else
 	{
-		for (j = 0; j <= src_count; j++)
-			dest[j] = src[j];
+		for (m = 0; m <= src_count; m++)
+		{
+			dest[m] = src[m];
+		}
+		
 		while (src_count <= n)
 			{
 				src_count++;
