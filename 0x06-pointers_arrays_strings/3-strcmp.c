9b40d1d745f4065aead515d88e113f7e7c3980cf
CzarProCoder <juliusczar36@gmail.com>
2022-12-29 01:39:14 -0800

    Function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	return (i - j);
+	while (*s1 && *s2 && *s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+	return (*s1 - *s2);
 }
