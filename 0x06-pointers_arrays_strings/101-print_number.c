0ee183597592f12aa8cd6c8995ff19dfd34f1ca7
CzarProCoder <juliusczar36@gmail.com>
2022-12-30 10:59:42 -0800

    Function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - Function that print an integer
+ * @n: Integer to be printed
+ */
+
+void print_number(int n)
+{
+	char a = n;
+
+	_putchar('a');
+	_putchar('\n');
+}
