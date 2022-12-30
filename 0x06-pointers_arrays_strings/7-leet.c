01f1d6486fc6fd0560af489cda3c925cbbcf78e9
CzarProCoder <juliusczar36@gmail.com>
2022-12-30 02:19:26 -0800

    Function to encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,12 +3,13 @@
 /**
  * leet - Function that encodes a string into 1337
  * @str: String to be encoded
+ * Return: Array of the news string
  */
 
 char *leet(char *str)
 {
-	int a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	int index, index2;
 
 	for (index = 0; str[index] != '\0'; index++)
