15717c5a2b49c3f5a1afe7512088d1d5e839c3fb
CzarProCoder <juliusczar36@gmail.com>
2022-12-30 10:28:36 -0800

    Function that encodes astring using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,12 +8,12 @@
 
 char *rot13(char *str)
 {
-	int real_l[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
 		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
 		'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
 		'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
 		'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	int rot_l[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
 		'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 		'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F',
@@ -22,11 +22,11 @@ char *rot13(char *str)
 
 	for (index = 0; str[index] != '\0'; index++)
 	{
-		for (index2 = 0; real_l[index2] != '\0'; index2++)
+		for (index2 = 0; a[index2] != '\0'; index2++)
 		{
-			if (str[index] == real_l[index2])
+			if (str[index] == a[index2])
 			{
-				str[index] = rot_l[index2];
+				str[index] = b[index2];
 			}
 		}
 	}
