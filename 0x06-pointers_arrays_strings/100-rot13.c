cff7931a11110b4efb0f6ef5be2656d1610c95c9
CzarProCoder <juliusczar36@gmail.com>
2022-12-30 10:05:41 -0800

    Function that encodes astring using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,8 +8,16 @@
 
 char *rot13(char *str)
 {
-	char real_l[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot_l[53] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int real_l[53] = {'a','b','c','d','e','f','g','h','i',
+		'j','k','l','m','n','o','p','q','r','s','t','u',
+		'v','w','x','y','z','A','B','C','D','E','F','G',
+		'H','I','J','K','L','M','N','O','P','Q','R','S',
+		'T','U','V','W','X','Y','Z'};
+	int rot_l[53] = {'n','o','p','q','r','s','t','u','v',
+		'w','x','y','z','a','b','c','d','e','f','g','h',
+		'i','j','k','l','m','N','O','P','Q','R','S','T',
+		'U','V','W','X','Y','Z','A','B','C','D','E','F',
+		'G','H','I','J','K','L','M'};
 	int index = 0;
 	int index2 = 0;
 
