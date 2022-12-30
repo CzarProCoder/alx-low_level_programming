237b161e0f37cab33733492cf05d319843566fe0
CzarProCoder <juliusczar36@gmail.com>
2022-12-30 03:59:16 -0800

    Function to decode a string using Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @str: String to be encoded
+ * Return: Encoded string
+ */
+
+char *rot13(char *str)
+{
+	int real_l[53] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	int rot_l[53] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	int index = 0;
+	int index2 = 0;
+
+	while (str[index] != '\0')
+	{
+		while (real_l[index2] != '\0')
+		{
+			if (str[index] == real_l[index2])
+			{
+				str[index] = rot_l[index2];
+			}
+			index2++;
+		}
+		index++;
+	}
+	return (str);
+}
