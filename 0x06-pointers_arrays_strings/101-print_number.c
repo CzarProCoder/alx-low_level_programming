ec0d3a8008fca319828ec9b91e40d841f739c346
CzarProCoder <juliusczar36@gmail.com>
2023-01-02 06:36:00 -0800

    Function that prints an interger

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,17 +11,23 @@ void print_number(int n)
 
 	if (n == 0)
 		_putchar('0');
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n -= n;
 	}
-	else if (n < 10)
-			_putchar(n + '0');
-	while (n < 10)
+	while (n > 0)
 	{
-		b = (n / 10);
-		_putchar(b + '0');
-		n /= 10;
+		if (n < 10)
+		{
+			_putchar(n + '0');
+			break;
+		}
+		else if (n > 10)
+		{
+			b = (n / 10);
+			_putchar(b + '0');
+			n /= 10;
+		}
 	}
 }
