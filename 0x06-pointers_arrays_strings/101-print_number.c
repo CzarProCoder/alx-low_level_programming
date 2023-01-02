d799de5257404928669ecdd9070783e148d9ef71
CzarProCoder <juliusczar36@gmail.com>
2023-01-01 23:02:16 -0800

    Function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -18,8 +18,13 @@ void print_number(int n)
 	}
 	while (n > 0)
 	{
-		b = (n / 10);
-		_putchar(b + '0');
-		n /= 10;
+		if (n < 10)
+			putchar(n + '0');
+		else
+		{
+			b = (n / 10);
+			_putchar(b + '0');
+			n /= 10;
+		}
 	}
 }
