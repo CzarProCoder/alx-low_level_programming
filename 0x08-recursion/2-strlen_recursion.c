ccdf2b250b137701e1f576f1aa268b73e0babe32
CzarProCoder <juliusczar36@gmail.com>
2023-01-05 08:30:35 -0800

    Function that prints a string in reverse

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: String to be checked
+ */
+
+int _strlen_recursion(char *s)
+{
+	unsigned int i = 0;
+
+	if ((*(s + i)) == '\0')
+		return (0);
+	return (i + strlen_recursion(s++));
+}
