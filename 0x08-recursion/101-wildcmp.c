4a532722fc73a85be83c971a3668ad0a3c8e3275
CzarProCoder <juliusczar36@gmail.com>
2023-01-07 02:46:26 -0800

    Functions that returns 1 is a string a palindromeand 0 if not

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 1 if identical; else 0.
+ */
+
+int wildcmp(char *s1, char *s2);
