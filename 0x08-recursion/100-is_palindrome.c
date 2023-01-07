1ef05300f369a861d13b4e6a1cfede2e19e9ef20
CzarProCoder <juliusczar36@gmail.com>
2023-01-07 03:04:07 -0800

    Functions that returns 1 is a string a palindromeand 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,10 +22,10 @@ int s_length(char *s)
 
 int comp_char(char *s, int s_len)
 {
+	if (s_len <= 0)
+		return (1);
 	if (*s != *(s + (s_len - 1)))
 		return (0);
-	if (s_len == 0)
-		return (1);
 	s_len -= 2;
 	s++;
 	return (comp_char(s, s_len));
