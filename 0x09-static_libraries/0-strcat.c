c1a117408e14c98e8aaced8ff4a1c6bf22339a37
Czarprocoder <juliusczar36@gmail.com>
2023-01-10 06:29:37 -0800

    Static and dynamic libraries in c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,3 +1,22 @@
 #include "main.h"
 
-char *_strcat(char *dest, char *src);
+/**
+ * _strcat - concatenates the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will append
+ *@src: string that will be concatenated upon
+ *
+ * Return: returns pointer to @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
 #include "main.h"
-
-int _abs(int n);
+/**
+ * _abs - computes absolute value of a value
+ * @c: integer
+ * description : computes integer
+ *
+ * Return: int
+ */
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		c = +c;
+	}
+	else
+	{
+		c = -c;
+	}
+	return (c);
+}
