e5f1a64ee83431e823351fd2b79094d9e4069d5f
Czarprocoder <juliusczar36@gmail.com>
2023-01-12 07:35:37 -0800

    Write a functin that concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strtow - Splits a string into words
+ * str: String to be split
+ * Return: Pointer to pointer
+ */
+
+char **strtow(char *str)
+{
+}
