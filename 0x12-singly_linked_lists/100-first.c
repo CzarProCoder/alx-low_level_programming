80d292f29e4a04817e9dda38c0ab4cb48d7ac7a2
Czarprocoder <juliusczar36@gmail.com>
2023-01-31 10:03:37 -0800

    Print before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * execute_before_main - executes this function before main function
- * Note: made possible by prior declaration of "__attribute__ ((constructor))"
+ * execute_before_main - fuction to execute before main
+ *	made possible by prior declaration of "__attribute__ ((constructor))"
  */
 
 void __attribute__ ((constructor)) execute_before_main()
