a80cd404952c54ca32b42829c81bf0a5cd5b96b6
Czarprocoder <juliusczar36@gmail.com>
2023-02-03 02:38:52 -0800

    Write a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Returns a new node at a given position
- * @head: Pointer to the list
- * @idx: Index where the node is to be added
- * @n: Number n to be added the node
- * Return: Address of the new node
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: pointer to head pointer of linked list
+ * @idx: index to insert new node
+ * @n: new node's data
+ * Return: address of new node, or NULL if failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	listint_t *new, *temp, *copy = *head;
 
+	unsigned int i = 0;
+	listint_t *new_node, *tmp;
+
+	/* account for empty pointer */
 	if (head == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	/* malloc and set values for new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
+	new_node->n = n;
 
+	/* account for idx 0 */
 	if (idx == 0)
 	{
-		new->next = copy;
-		*head = new;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 
-	while (copy && count <= idx)
+	/* iterate to 1 before nth index to insert */
+	tmp = *head;
+	while (i < (idx - 1))
 	{
-		if ((count + 1) == idx)
+		tmp = tmp->next;
+		i++;
+
+		if (tmp == NULL) /* account for idx out of range */
 		{
-			temp = copy->next;
-			copy->next = new;
-			new->next = temp;
+			free(new_node);
+			return (NULL);
 		}
-		copy = copy->next;
-		count++;
 	}
-	return (new);
+
+	/* link into list */
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
 }
