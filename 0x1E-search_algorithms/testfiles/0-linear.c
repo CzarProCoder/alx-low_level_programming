ddceb375448a911cfafa98a7287c9cc22af8cc46
CzarProCoder <juliusczar36@gmail.com>
2023-07-11 15:09:58 +0300

    Created a binary search algorithm to search for a value in a sorted array

---
+++ 0x1E-search_algorithms/testfiles/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * linear_search - Function to search for a value in an array of integers
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ * Return:	Success: First index where the value is located
+ *		Failure: Returns -1 if array is absent or NULL
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+
+	if (array == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", index, *array);
+		if (*(array + index) == value)
+			return (index);
+		array++;
+	}
+	return (-1);
+}
